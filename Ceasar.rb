129b3de8b99a8dfd4e951d85f36419cba5a3f687
Jacob Haflett <hafletjm01@gmail.com>
2023-10-08 17:34:10 -0400

    lol i forgot to commit this stuff oops

---
+++ Ceasar.rb
@@ -0,0 +1,20 @@
+puts "Please input a message to encode:\n"
+message = gets.chomp
+#using chomp because we want input on the keypress enter
+puts "Please input number cipher to use: \n"
+cipher = gets.chomp.to_i
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j',klmnopqrstuvwxyz]
+
+
+# find the location of the letter in the alphabet
+#
+# Then Check to see if the cipher key and the location is larger then the alphabet
+# if it is then loop over from A starting at 26 plus the key minus the current letters position.
+#
+  
+end
+
+puts message
+
+puts cipher
